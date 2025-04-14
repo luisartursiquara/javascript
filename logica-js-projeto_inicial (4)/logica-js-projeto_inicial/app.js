5d84cf03d08482c62bc42da3725f41351463f014
luisartursiquara <lasiquara@gmail.com>
2025-04-14 01:58:45 +0100

    atualizacoes javascript

---
+++ logica-js-projeto_inicial (4)/logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert("Boas vindas ao jogo do numero secreto");
+let numeroSecreto = 29;
+let chute = prompt ("Escolha um numero entre 1 e 30");
+
+if (chute == numeroSecreto) {
+    console.log("isso ai! voce descobriu o numero secreto");
+}
+
